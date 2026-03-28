fdd39728e3fc9a86d4d583242ebb31d32353e722
HunterRaven <hunterraven2010@gmail.com>
2026-03-28 00:37:07 -0400

    With Josh's stuff

--- flake.nix
+++ flake.nix
@@ -1,29 +1,62 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-        home-manager.url = "github:rycee/home-manager/release-25.11";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:rycee/home-manager/release-25.11";
+    common.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/common";
+    ros_neovim.url = "git+https://git.joshuabell.xyz/ringofstorms/nvim";
+    opencode.url = "github:anomalyco/opencode/0dcdf5f529dced23d8452c9aa5f166abb24d8f7c";
+  };
 
-    outputs =
-    { nixpkgs, home-manager, ... }:
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      common,
+      ros_neovim,
+      opencode,
+      ...
+    }:
     let
-        hostname = "hermes";
-        primaryUser = "aaron";
-        lib = nixpkgs.lib;
+      hostname = "hermes";
+      primaryUser = "aaron";
+      lib = nixpkgs.lib;
     in
     {
-        nixosConfigurations = {
-            "${hostname}" = lib.nixosSystem {
-                modules = [
-                    #nixosModule
-                    ./configuration.nix
-                    ./hardware-configuration.nix
-                    ./aaron.nix
+      nixosConfigurations = {
+        "${hostname}" = lib.nixosSystem {
+          modules = [
+            #nixosModule
+            ./configuration.nix
+            ./hardware-configuration.nix
+            ./aaron.nix
+            home-manager.nixosModules.default
+            ({
+              home-manager.sharedModules = [
+                common.homeManagerModules.zsh
+                common.homeManagerModules.foot
+                common.homeManagerModules.direnv
+                common.homeManagerModules.starship
+                common.homeManagerModules.zoxide
+                common.homeManagerModules.tmux
+              ];
+            })
+            common.nixosModules.zsh
+            common.nixosModules.jetbrains_font
+            common.nixosModules.essentials
+            ros_neovim.nixosModules.default
+            (
+              { pkgs, ... }:
+              {
+                ringofstorms-nvim = {
+                  includeAllRuntimeDependencies = true;
+                  underPrefix = "jnvim";
+                };
+                environment.systemPackages = [ opencode.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+              }
+            )
+          ];
 
-                home-manager.nixosModules.default
-                ];
-
-            };
         };
+      };
     };
 }
